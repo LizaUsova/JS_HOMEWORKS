718817b06c532352a3bc0a4fa5b853c942d2a6fa
Liza Usova <Liza.usova@gmail.com>
2023-12-20 05:22:42 +0200

    HW 4 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+//task 1
+const color = prompt('write the color');
+const country = prompt('write the name of the country');
+const animal = prompt('write the name of the animal');
+
+console.log ('user input is: ' + color + country + animal);
+
+//task 2
+const number = 88888;
+const numberString = number.toString();
+
+const firstChar = numberString [0];
+const secondChar = numberString [1];
+const thirdChar = numberString [2];
+const fourthChar = numberString [2];
+const lastChar = numberString [numberString.length - 1];
+console.log (firstChar+ ' ' +secondChar+ ' ' +thirdChar+ ' ' +fourthChar+ ' ' +lastChar);
