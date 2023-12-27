39aa8d5396251eb2c075619b8586520903797a3f
Liza Usova <Liza.usova@gmail.com>
2023-12-28 00:42:05 +0200

    HW 12 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+let numberFirst = prompt('Write the first number');
+let numberSecond = prompt('Write the second number');
+
+numberFirst = parseFloat(numberFirst);
+numberSecond = parseFloat(numberSecond);
+
+if (isNaN(numberFirst) || isNaN(numberSecond)) {
+    alert('Please enter valid numbers');
+} else {
+    let sum = numberFirst + numberSecond;
+    let difference = numberFirst - numberSecond;
+    let product = numberFirst * numberSecond;
+    let quotient = numberFirst / numberSecond;
+
+    alert(
+        'Results:\n' +
+        numberFirst + ' + ' + numberSecond + ' = ' + sum + '\n' +
+        numberFirst + ' - ' + numberSecond + ' = ' + difference + '\n' +
+        numberFirst + ' * ' + numberSecond + ' = ' + product + '\n' +
+        numberFirst + ' / ' + numberSecond + ' = ' + quotient
+    );
+}
+
+
