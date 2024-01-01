850ef3ebece96be25fa95bee4e1552faa3febe44
Liza Usova <Liza.usova@gmail.com>
2024-01-02 01:50:32 +0200

    hw_14 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let userInput = prompt('Enter number of hours');
+
+userInput = parseFloat(userInput);
+
+if (isNaN (userInput)) {
+    alert('Please enter valid numbers');
+} else {
+    let numberOfSeconds = userInput * 60 * 60;
+    alert('Result: ' + numberOfSeconds + ' seconds in ' + userInput + ' hours');
+}
