4198b21e39cab0291f5a790c5e1b12f09bd5683a
Liza Usova <Liza.usova@gmail.com>
2024-01-02 02:01:22 +0200

    hw_15 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+alert('To calculate the arithmetic average, enter the numbers in the following input fields.');
+let numberFirst = prompt('Write the first number');
+let numberSecond = prompt('Write the second number');
+let numberThird = prompt('Write the third number');
+
+numberFirst = parseFloat(numberFirst);
+numberSecond = parseFloat(numberSecond);
+numberThird = parseFloat(numberThird);
+
+if (isNaN(numberFirst) || isNaN(numberSecond) || isNaN(numberThird)) {
+    alert('Please enter valid numbers');
+} else {
+    let arithmeticAverage = (numberFirst + numberSecond + numberThird) / 3;
+    alert('Result: ' + arithmeticAverage);
+}
