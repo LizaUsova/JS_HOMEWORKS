b36a8e2c543b296b0f7469f1235646863ff25d65
Liza Usova <Liza.usova@gmail.com>
2024-01-09 23:40:58 +0200

    hw_17 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let numOrStr = prompt('input number or string');
+console.log(typeof numOrStr)
+
+switch (true) {
+    case numOrStr === null:
+        alert('ви скасували');
+        break;
+    case numOrStr === '':
+        alert('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        alert('number is Ba_NaN')
+        break
+    default:
+        alert('OK!');
+}
